7749af1891bca38eb00be6fb193286981dc03a8e
Jesus martin romero <jesusmartinromero10@gmail.com>
2024-03-28 10:59:45 +0100

    creo rutas y seguridad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: PagesModule
+  },
+  {
+    path: 'pages',
+    loadChildren: ()=>import('./pages/pages.module').then((m)=>m.PagesModule)
+  },
+  {
+    path: 'auth',
+    loadChildren:()=>import('./auth/auth.module').then((m)=>m.AuthModule)
+
+  },
+  {
+    path:'**',
+    component:PagesModule
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,15 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeaListComponent } from './tea-list/tea-list.component';
 import { TeaDetailComponent } from './tea-detail/tea-detail.component';
+import { RouterModule, Routes } from '@angular/router';
+import { guardGuard } from '../guards/guard.guard';
 
 
+const routes: Routes = [
+  {
+    path: 'tea-list',
+    component: TeaListComponent,
+    canActivate:[guardGuard]
+    
+  },
+  {
+    path:'tea-detail/:id',
+    component:TeaDetailComponent,
+    canActivate:[guardGuard]
 
+  },
+  {
+    path:'**',
+    component:TeaListComponent
+  }
+]
 @NgModule({
   declarations: [
     TeaListComponent,
-    TeaDetailComponent
+    TeaDetailComponent,
   ],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ],
   exports: [
